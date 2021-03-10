a20dc24623a8acd8de3c8e8a20a638ced3c2f90e
Emily Åkerström <emily.akerstrom@gmail.com>
2021-03-10 04:58:30 +0100

    Anchor storage backend done?

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from redis import Redis
+import json
+
+
+app = Flask(__name__)
+r = Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+@app.route('/delete_anchor', methods=['POST'])
+def delete_anchor():
+    sensorID = request.form["SensorID"]
+    r.delete(sensorID)
+    return 'Deleted anchor: "%s"' % sensorID
+    
+@app.route('/get_anchors')
+def get_anchors():
+    keys = r.scan()[1]
+    vals = r.mget(keys)
+    return dict(zip(keys, vals))
+
+@app.route('/get_anchor', methods=['POST'])
+def get_anchor():
+    return r.get(request.form["SensorID"])
+
+@app.route('/set_anchor', methods=['POST'])
+def set_anchor():
+    sensorID = request.form["SensorID"]
+    anchorID = request.form["AnchorID"]
+    r.set(sensorID, anchorID)
+    return 'Created anchor: {%s: %s}' % (sensorID, anchorID)
+
